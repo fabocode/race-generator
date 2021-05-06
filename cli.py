e77a074fe151706cd724ee4758adaabcca44e94b
Fabian Barraez <fabianbarraez@gmail.com>
2021-05-06 16:38:09 -0500

    feat: first entry source to call application

--- cli.py
+++ cli.py
@@ -1,10 +1,5 @@
-import click
+from race_generator.__main__ import main
 
-
-@click.command()
-@click.option('--as-cowboy', '-c', is_flag=True, help='Greet as a cowboy.')
-@click.argument('name', default='world', required=False)
-def main(name, as_cowboy):
-    """A Python program to take user inputs to generate waypoints in pulses/gps for racing applications"""
-    greet = 'Howdy' if as_cowboy else 'Hello'
-    click.echo('{0}, {1}.'.format(greet, name))
+if __name__ == '__main__':
+    main()
+    
